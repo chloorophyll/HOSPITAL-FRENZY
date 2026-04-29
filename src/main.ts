4917585a4eef4d42ca28058737d7b833c1ba30fd
chloorophyll <franceschloe.talaman-23@cpu.edu.ph>
2026-04-29 14:41:54 +0800

    add Phaser scene with moving sprite

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,49 @@
-import { TriageNurse } from './classes/triagenurse'
-import { WardNurse } from './classes/wardnurse'
-import { GeneralPractitioner } from './classes/generalpractitioner'
-import { ERDoctor } from './classes/ERdoctor'
+import * as Phaser from 'phaser'
 
-const triage = new TriageNurse('Nurse Joy')
-const ward = new WardNurse('Nurse Ana')
-const gp = new GeneralPractitioner('Dr. Santos')
-const er = new ERDoctor('Dr. Reyes')
+class GameScene extends Phaser.Scene {
+  private player!: Phaser.Physics.Arcade.Sprite
+  private cursors!: Phaser.Types.Input.Keyboard.CursorKeys
 
-const testPatient = { name: 'Juan dela Cruz' }
+  constructor() {
+    super({ key: 'GameScene' })
+  }
 
-triage.treat(testPatient)
-triage.assess(testPatient)
-ward.treat(testPatient)
-gp.treat(testPatient)
-er.treat(testPatient)
+  preload() {
+    this.load.image('player', 'assets/player1.png')
+  }
+
+  create() {
+    this.cameras.main.setBackgroundColor('#1a1a2e')
+
+    this.player = this.physics.add.sprite(400, 300, 'player')
+    this.player.setScale(2)
+
+    this.cursors = this.input.keyboard!.createCursorKeys()
+
+    this.add.text(10, 10, 'Arrow keys to move!', {
+      fontSize: '16px',
+      color: '#ffffff'
+    })
+  }
+
+  update() {
+    const body = this.player.body as Phaser.Physics.Arcade.Body
+    body.setVelocity(0)
+
+    if (this.cursors.left.isDown)  body.setVelocityX(-200)
+    if (this.cursors.right.isDown) body.setVelocityX(200)
+    if (this.cursors.up.isDown)    body.setVelocityY(-200)
+    if (this.cursors.down.isDown)  body.setVelocityY(200)
+  }
+}
+
+new Phaser.Game({
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: { debug: false }
+  },
+  scene: [GameScene]
+})
